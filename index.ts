91c4c0b0abdfb346eb55df75c3451ab9bb5e66c3
Passam <119688260+Passam02@users.noreply.github.com>
2023-03-09 15:58:16 +0100

    Add option to create points to lists

--- index.ts
+++ index.ts
@@ -1,18 +1,27 @@
 const mainButton = document.querySelector('#mainButton')!
 const todoLists = document.querySelector('.todoLists')!
 
+
 mainButton.addEventListener('click', function () {
     const div = document.createElement('div')
     const heading = document.createElement('h3')
     heading.innerText = 'List Name'
     heading.className = 'text-center'
-    const button = document.createElement('button')
+    const a = document.createElement('a')
+    a.innerHTML = '<a class="todoList text-center listButton"><div><svg class="m-2" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/></svg></div></a>'
+    a.addEventListener('click', function () {
+        const inp = document.createElement('input')
+        inp.addEventListener('change', function () {
+            let text = inp.value
+            const li = document.createElement('li')
+            li.innerText = text
+            inp.parentElement?.insertBefore(li, inp)
+            inp.remove()
+        })
+        a.parentElement?.insertBefore(inp, a)
+    })
     div.append(heading)
-    div.append(button)
-    div.className = 'todoList d-flex'
-    console.log(div)
-    console.dir(div)
-    console.log(todoLists)
+    div.append(a)
+    div.className = 'todoList d-flex flex-column'
     todoLists.insertBefore(div, mainButton)
 })
-
