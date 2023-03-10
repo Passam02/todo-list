ffdf6fb07b58e321f3d39d10e70c47c90dc2490c
Passam <119688260+Passam02@users.noreply.github.com>
2023-03-10 15:03:35 +0100

    Add style to lists

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ mainButton.addEventListener('click', function () {
     var div = document.createElement('div');
     var heading = document.createElement('h3');
     heading.innerText = 'List Name';
-    heading.className = 'text-center';
+    heading.className = 'text-center mt-2 border-bottom border-dark';
     var a = document.createElement('a');
     a.innerHTML = '<a class="todoList text-center listButton"><div><svg class="m-2" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/></svg></div></a>';
     a.addEventListener('click', function () {
         var _a;
         var inp = document.createElement('input');
+        inp.placeholder = 'Type Here...';
         inp.addEventListener('change', function () {
             var _a;
             var text = inp.value;
             var li = document.createElement('li');
             li.innerText = text;
+            li.style = 'overflow-wrap: anywhere';
+            li.className = 'm-2';
             (_a = inp.parentElement) === null || _a === void 0 ? void 0 : _a.insertBefore(li, inp);
             inp.remove();
         });
@@ -23,8 +26,5 @@ mainButton.addEventListener('click', function () {
     div.append(heading);
     div.append(a);
     div.className = 'todoList d-flex flex-column';
-    console.log(div);
-    console.dir(div);
-    console.log(todoLists);
     todoLists.insertBefore(div, mainButton);
 });
--- index.ts
+++ index.ts
@@ -6,15 +6,18 @@ mainButton.addEventListener('click', function () {
     const div = document.createElement('div')
     const heading = document.createElement('h3')
     heading.innerText = 'List Name'
-    heading.className = 'text-center'
+    heading.className = 'text-center mt-2 border-bottom border-dark'
     const a = document.createElement('a')
     a.innerHTML = '<a class="todoList text-center listButton"><div><svg class="m-2" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/></svg></div></a>'
     a.addEventListener('click', function () {
         const inp = document.createElement('input')
+        inp.placeholder = 'Type Here...'
         inp.addEventListener('change', function () {
             let text = inp.value
             const li = document.createElement('li')
             li.innerText = text
+            li.style = 'overflow-wrap: anywhere'
+            li.className = 'm-2'
             inp.parentElement?.insertBefore(li, inp)
             inp.remove()
         })
