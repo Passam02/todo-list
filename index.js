f408231f1f0b7041709395f838c1d42d19e1afee
Passam <119688260+Passam02@users.noreply.github.com>
2023-03-22 16:05:38 +0100

    Add margin to inputs

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ mainButton.addEventListener('click', function () {
         aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>';
         aDelete.addEventListener('click', function () { return remove(aDelete); });
         inp.placeholder = 'Type Here...';
-        inp.className = 'flex-grow-1';
+        inp.className = 'flex-grow-1 mt-1';
         inp.addEventListener('change', function () {
             var _a, _b, _c;
             var div = document.createElement('div');
