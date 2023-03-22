21faa71679d502985c590b42c3ad3baff43d56f9
Passam <119688260+Passam02@users.noreply.github.com>
2023-03-22 18:28:27 +0100

    Save todo lists in array

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ mainButton.addEventListener('click', function () {
     var id = getuuid();
     var newTodo = {
         id: id,
-        heading: ''
+        heading: null
     };
     todos.push(newTodo);
     console.log(todos);
     var mainId = document.createElement('input');
     mainId.value = id;
     mainId.style.cssText = "display: none";
-    var div = document.createElement('div');
+    var mainDiv = document.createElement('div');
     var divCol = document.createElement('div');
     divCol.className = 'col';
     var divHead = document.createElement('div');
@@ -62,8 +62,8 @@ mainButton.addEventListener('click', function () {
     aDelete.className = 'p-1';
     aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>';
     aDelete.addEventListener('click', function () {
-        var _a, _b, _c, _d;
-        (_d = (_c = (_b = (_a = aDelete.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement) === null || _c === void 0 ? void 0 : _c.parentElement) === null || _d === void 0 ? void 0 : _d.remove();
+        divCol.remove();
+        todos = todos.filter(function (c) { return c.id !== mainId.value; });
     });
     var aEdit = document.createElement('a');
     aEdit.style.cssText = "display: none";
@@ -81,47 +81,54 @@ mainButton.addEventListener('click', function () {
     divHead.append(divButtons);
     a.innerHTML = '<a class="todoList text-center listButton"><div><svg class="m-2" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/></svg></div></a>';
     a.addEventListener('click', function () {
-        var _a;
         var list = todos.find(function (c) { return c.id === mainId.value; });
         var pointId = getuuid();
-        list.li = {
-            id: pointId,
-            text: '',
+        list[pointId] = {
+            idp: pointId,
+            text: null,
             checked: false
         };
+        var liId = document.createElement('input');
+        liId.value = pointId;
+        liId.style.cssText = "display: none";
         var inp = document.createElement('input');
-        var div = document.createElement('div');
-        div.className = 'd-flex align-items-center';
+        var liDiv = document.createElement('div');
+        liDiv.className = 'd-flex align-items-center';
         var aDelete = document.createElement('a');
         aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>';
-        aDelete.addEventListener('click', function () { return remove(aDelete); });
+        aDelete.addEventListener('click', function () {
+            liDiv.remove();
+            delete list[pointId];
+        });
         inp.placeholder = 'Type Here...';
         inp.className = 'flex-grow-1 mt-1';
         inp.addEventListener('change', function () {
-            var _a, _b, _c;
+            var thisId = pointId;
             var div = document.createElement('div');
             div.className = 'border-bottom border-dark d-flex align-items-center p-2';
             var aDelete = document.createElement('a');
             aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>';
-            aDelete.addEventListener('click', function () { return remove(aDelete); });
+            aDelete.addEventListener('click', function () {
+                div.remove();
+                delete list[thisId];
+            });
             var aChecked = document.createElement('a');
             aChecked.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>';
             aChecked.style.cssText = "display: none";
             aChecked.addEventListener('click', function () {
-                var _a, _b, _c;
-                (_c = (_b = (_a = aChecked === null || aChecked === void 0 ? void 0 : aChecked.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.firstChild) === null || _c === void 0 ? void 0 : _c.style = '';
+                li.style = '';
                 aChecked.style.cssText = 'display: none';
                 aCheck.style.cssText = 'dislay: block';
             });
             var aCheck = document.createElement('a');
             aCheck.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>';
             aCheck.addEventListener('click', function () {
-                var _a, _b, _c, _d;
-                (_c = (_b = (_a = aCheck === null || aCheck === void 0 ? void 0 : aCheck.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.firstChild) === null || _c === void 0 ? void 0 : _c.style = 'text-decoration: line-through';
-                (_d = aCheck.parentElement) === null || _d === void 0 ? void 0 : _d.append(aChecked);
+                li.style = 'text-decoration: line-through';
+                buttons.append(aChecked);
                 aCheck.style.cssText = 'display: none';
                 aChecked.style.cssText = 'display: block';
             });
+            list[thisId].text = inp.value;
             var text = inp.value;
             var li = document.createElement('li');
             li.innerText = text;
@@ -134,18 +141,19 @@ mainButton.addEventListener('click', function () {
             buttons.append(aChecked);
             buttons.className = 'd-flex flex-column';
             div.append(buttons);
-            (_b = (_a = inp.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.insertBefore(div, inp.parentElement);
-            (_c = inp.parentElement) === null || _c === void 0 ? void 0 : _c.remove();
+            mainDiv.insertBefore(div, liDiv);
+            liDiv.remove();
         });
-        div.append(inp);
+        liDiv.append(liId);
+        liDiv.append(inp);
         var divDelete = document.createElement('div');
         divDelete.append(aDelete);
-        div.append(divDelete);
-        (_a = a.parentElement) === null || _a === void 0 ? void 0 : _a.insertBefore(div, a);
+        liDiv.append(divDelete);
+        mainDiv.insertBefore(liDiv, a);
     });
-    div.append(divHead);
-    div.append(a);
-    div.className = 'todoList card d-flex flex-column';
-    divCol.append(div);
+    mainDiv.append(divHead);
+    mainDiv.append(a);
+    mainDiv.className = 'todoList card d-flex flex-column';
+    divCol.append(mainDiv);
     todoLists.insertBefore(divCol, mainButton.parentElement);
 });
--- index.ts
+++ index.ts
@@ -7,16 +7,16 @@ return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, variable_name =>
 }
 interface Todo {
     id: string
-    heading: string;  
+    heading: string | null;  
 }
 
 interface Todo extends Record<string,any> {
         idp?: string
-        text?: string
+        text?: string | null
         checked?: boolean
 }
 
-const todos: Todo[] = []
+var todos: Todo[] = []
 
 const mainButton = document.querySelector('#mainButton')! as HTMLButtonElement
 const todoLists = document.querySelector('.todoLists')! as HTMLDivElement
@@ -28,15 +28,14 @@ mainButton.addEventListener('click', function () {
     const id = getuuid()
     const newTodo: Todo = {
         id: id,
-        heading: '',
-        }
-    
+        heading: null,
+    }
     todos.push(newTodo)
     console.log(todos)
     const mainId = document.createElement('input')
     mainId.value = id
     mainId.style.cssText = "display: none"
-    const div = document.createElement('div')
+    const mainDiv = document.createElement('div')
     const divCol = document.createElement('div')
     divCol.className = 'col'
     const divHead = document.createElement('div')
@@ -76,7 +75,8 @@ mainButton.addEventListener('click', function () {
     aDelete.className = 'p-1'
     aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>'
     aDelete.addEventListener('click', function () {
-        aDelete.parentElement?.parentElement?.parentElement?.parentElement?.remove()
+        divCol.remove()
+        todos = todos.filter(c => c.id !== mainId.value)
     })
     const aEdit = document.createElement('a')
     aEdit.style.cssText = "display: none"
@@ -94,44 +94,55 @@ mainButton.addEventListener('click', function () {
     divHead.append(divButtons)
     a.innerHTML = '<a class="todoList text-center listButton"><div><svg class="m-2" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/></svg></div></a>'
     a.addEventListener('click', function () {
-        const list = todos.find(c => c.id === mainId.value)
+        const list = todos.find(c => {return c.id === mainId.value})
         const pointId = getuuid()
-        list!.li = {
-            id: pointId,
-            text: '',
+        list![pointId] = {
+            idp: pointId,
+            text: null,
             checked: false
         }
+        const liId = document.createElement('input')
+        liId.value = pointId
+        liId.style.cssText = "display: none"
         const inp = document.createElement('input')
-        const div = document.createElement('div')
-        div.className ='d-flex align-items-center'
+        const liDiv = document.createElement('div')
+        liDiv.className ='d-flex align-items-center'
         const aDelete = document.createElement('a')
         aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>'
-        aDelete.addEventListener('click', () => remove(aDelete) )
+        aDelete.addEventListener('click', () => {
+            liDiv.remove()
+            delete list![pointId]
+        })
         inp.placeholder = 'Type Here...'
         inp.className = 'flex-grow-1 mt-1'
         inp.addEventListener('change', function () {
+            
+            const thisId = pointId
             const div = document.createElement('div')
             div.className = 'border-bottom border-dark d-flex align-items-center p-2'
             const aDelete = document.createElement('a')
             aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>'
-            aDelete.addEventListener('click', () => remove(aDelete))
+            aDelete.addEventListener('click', () => {
+                div.remove()
+                delete list![thisId]
+            })
             const aChecked = document.createElement('a')
             aChecked.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>'
             aChecked.style.cssText = "display: none"
             aChecked.addEventListener('click', () => {
-                aChecked?.parentElement?.parentElement?.firstChild?.style = ''
+                li.style = ''
                 aChecked.style.cssText = 'display: none'
                 aCheck.style.cssText = 'dislay: block'
             })
             const aCheck = document.createElement('a')
             aCheck.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/></svg>'
             aCheck.addEventListener('click', () => {
-                aCheck?.parentElement?.parentElement?.firstChild?.style = 'text-decoration: line-through'
-                aCheck.parentElement?.append(aChecked)
+                li.style = 'text-decoration: line-through'
+                buttons.append(aChecked)
                 aCheck.style.cssText = 'display: none'
                 aChecked.style.cssText = 'display: block'
             })
-
+            list![thisId].text = inp.value
             let text = inp.value
             const li = document.createElement('li')
             li.innerText = text
@@ -144,18 +155,19 @@ mainButton.addEventListener('click', function () {
             buttons.append(aChecked)
             buttons.className = 'd-flex flex-column'
             div.append(buttons)
-            inp.parentElement?.parentElement?.insertBefore(div, inp.parentElement)
-            inp.parentElement?.remove()
+            mainDiv.insertBefore(div, liDiv)
+            liDiv.remove()
         })
-        div.append(inp)
+        liDiv.append(liId)
+        liDiv.append(inp)
         const divDelete = document.createElement('div')
         divDelete.append(aDelete)
-        div.append(divDelete)
-        a.parentElement?.insertBefore(div, a)
+        liDiv.append(divDelete)
+        mainDiv.insertBefore(liDiv, a)
     })
-    div.append(divHead)
-    div.append(a)
-    div.className = 'todoList card d-flex flex-column'
-    divCol.append(div)
+    mainDiv.append(divHead)
+    mainDiv.append(a)
+    mainDiv.className = 'todoList card d-flex flex-column'
+    divCol.append(mainDiv)
     todoLists.insertBefore(divCol, mainButton.parentElement)
 })
