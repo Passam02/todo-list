31681eae21b8d5d9e2728009bd1a55f2f984c302
Passam <119688260+Passam02@users.noreply.github.com>
2023-03-23 11:52:50 +0100

    fix inputs style

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ function getuuid() {
 var todos = [];
 var mainButton = document.querySelector('#mainButton');
 var todoLists = document.querySelector('.todoLists');
-var remove = function (x) {
-    var _a, _b;
-    (_b = (_a = x.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.remove();
-};
 mainButton.addEventListener('click', function () {
     var id = getuuid();
     var newTodo = {
@@ -95,13 +91,13 @@ mainButton.addEventListener('click', function () {
         var liDiv = document.createElement('div');
         liDiv.className = 'd-flex align-items-center';
         var aDelete = document.createElement('a');
-        aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>';
+        aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>';
         aDelete.addEventListener('click', function () {
             liDiv.remove();
             delete list[pointId];
         });
         inp.placeholder = 'Type Here...';
-        inp.className = 'flex-grow-1 mt-1';
+        inp.className = 'flex-grow-1 mt-1 ms-1';
         inp.addEventListener('change', function () {
             var thisId = pointId;
             var div = document.createElement('div');
--- index.ts
+++ index.ts
@@ -21,9 +21,6 @@ var todos: Todo[] = []
 const mainButton = document.querySelector('#mainButton')! as HTMLButtonElement
 const todoLists = document.querySelector('.todoLists')! as HTMLDivElement
 
-const remove = (x: HTMLElement) => {
-    x.parentElement?.parentElement?.remove()
-}
 mainButton.addEventListener('click', function () {
     const id = getuuid()
     const newTodo: Todo = {
@@ -108,13 +105,13 @@ mainButton.addEventListener('click', function () {
         const liDiv = document.createElement('div')
         liDiv.className ='d-flex align-items-center'
         const aDelete = document.createElement('a')
-        aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>'
+        aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>'
         aDelete.addEventListener('click', () => {
             liDiv.remove()
             delete list![pointId]
         })
         inp.placeholder = 'Type Here...'
-        inp.className = 'flex-grow-1 mt-1'
+        inp.className = 'flex-grow-1 mt-1 ms-1'
         inp.addEventListener('change', function () {
             
             const thisId = pointId
