7edea07d164cf90442abd43b8814d53530296e65
Passam <119688260+Passam02@users.noreply.github.com>
2023-03-23 13:17:13 +0100

    Create ts interface for todo lists

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ mainButton.addEventListener('click', function () {
     var id = getuuid();
     var newTodo = {
         id: id,
-        heading: null
+        heading: null,
+        points: {}
     };
     todos.push(newTodo);
     console.log(todos);
@@ -79,8 +80,7 @@ mainButton.addEventListener('click', function () {
     a.addEventListener('click', function () {
         var list = todos.find(function (c) { return c.id === mainId.value; });
         var pointId = getuuid();
-        list[pointId] = {
-            idp: pointId,
+        list.points[pointId] = {
             text: null,
             checked: false
         };
@@ -94,7 +94,7 @@ mainButton.addEventListener('click', function () {
         aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>';
         aDelete.addEventListener('click', function () {
             liDiv.remove();
-            delete list[pointId];
+            delete list.points[pointId];
         });
         inp.placeholder = 'Type Here...';
         inp.className = 'flex-grow-1 mt-1 ms-1';
@@ -106,7 +106,7 @@ mainButton.addEventListener('click', function () {
             aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>';
             aDelete.addEventListener('click', function () {
                 div.remove();
-                delete list[thisId];
+                delete list.points[thisId];
             });
             var aChecked = document.createElement('a');
             aChecked.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>';
@@ -124,7 +124,7 @@ mainButton.addEventListener('click', function () {
                 aCheck.style.cssText = 'display: none';
                 aChecked.style.cssText = 'display: block';
             });
-            list[thisId].text = inp.value;
+            list.points[thisId].text = inp.value;
             var text = inp.value;
             var li = document.createElement('li');
             li.innerText = text;
--- index.ts
+++ index.ts
@@ -5,17 +5,16 @@ return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, variable_name =>
 (variable_name ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> variable_name / 4).toString(16)
 );
 }
-interface Todo {
+interface Todo { 
+    points: { 
+        [key: string]: {
+            text: string | null
+            checked: boolean
+        }
+    },
     id: string
-    heading: string | null;  
+    heading: string | null; 
 }
-
-interface Todo extends Record<string,any> {
-        idp?: string
-        text?: string | null
-        checked?: boolean
-}
-
 var todos: Todo[] = []
 
 const mainButton = document.querySelector('#mainButton')! as HTMLButtonElement
@@ -26,6 +25,7 @@ mainButton.addEventListener('click', function () {
     const newTodo: Todo = {
         id: id,
         heading: null,
+        points: {}
     }
     todos.push(newTodo)
     console.log(todos)
@@ -93,8 +93,7 @@ mainButton.addEventListener('click', function () {
     a.addEventListener('click', function () {
         const list = todos.find(c => {return c.id === mainId.value})
         const pointId = getuuid()
-        list![pointId] = {
-            idp: pointId,
+        list!.points![pointId as keyof Todo["points"]] = {
             text: null,
             checked: false
         }
@@ -108,7 +107,7 @@ mainButton.addEventListener('click', function () {
         aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>'
         aDelete.addEventListener('click', () => {
             liDiv.remove()
-            delete list![pointId]
+            delete list!.points![pointId as keyof Todo]
         })
         inp.placeholder = 'Type Here...'
         inp.className = 'flex-grow-1 mt-1 ms-1'
@@ -121,7 +120,7 @@ mainButton.addEventListener('click', function () {
             aDelete.innerHTML = '<svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16"><path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/></svg>'
             aDelete.addEventListener('click', () => {
                 div.remove()
-                delete list![thisId]
+                delete list!.points![thisId as keyof Todo]
             })
             const aChecked = document.createElement('a')
             aChecked.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/></svg>'
@@ -139,7 +138,7 @@ mainButton.addEventListener('click', function () {
                 aCheck.style.cssText = 'display: none'
                 aChecked.style.cssText = 'display: block'
             })
-            list![thisId].text = inp.value
+            list!.points![thisId as keyof Todo].text = inp.value
             let text = inp.value
             const li = document.createElement('li')
             li.innerText = text
